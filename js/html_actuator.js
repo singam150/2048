6160b59e1395dd1127f1e2977d6b276453e9e004
srinivas <srinivas.singam1255@gmail.com>
2016-07-12 10:47:44 +0530

    images instead of number

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -62,7 +62,11 @@ HTMLActuator.prototype.addTile = function (tile) {
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
-  inner.textContent = tile.value;
+  // inner.textContent = tile.value;
+  img = document.createElement("img");
+  img.setAttribute("src", "images/image_"+tile.value+".jpg");
+  img.setAttribute("style", "height:106.25px; width: 106.25px");
+  inner.appendChild(img);
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
